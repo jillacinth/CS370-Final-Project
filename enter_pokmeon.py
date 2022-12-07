be62fa1ad41770fb80b79e3d7c961c3b30bcf5c1
jill3 <jill30jack@gmail.com>
2022-12-07 01:28:24 -0500

    trying to make methods seperately

--- enter_pokmeon.py
+++ enter_pokmeon.py
@@ -1,8 +1,12 @@
-
+import pandas as pd
 
 class Check_Pokemon:
-    def __init__(self, pokemon):
+    def __init__(self, pokemon, data):
         self.pokemon = pokemon
+        self.data = data
 
-    
-    
+    def loadstats(self, name):
+        id_num = self.data.index(name)
+        hp = self.data['HP']
+
+
